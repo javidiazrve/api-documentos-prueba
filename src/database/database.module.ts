ba2ecfdefd952e23210b309d6cc6d482979adc20
javidiazrve <68872873+javidiazrve@users.noreply.github.com>
2023-05-31 22:08:51 -0300

    mongo env variables config

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,10 +1,17 @@
 import { Global, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Global()
 @Module({
     imports: [
-        MongooseModule.forRoot('mongodb://localhost/api-documentos'),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+              uri: `mongodb+srv://${configService.get<string>('MONGO_USER')}:${configService.get<string>('MONGO_PASSWORD')}@api-documents.oni8jih.mongodb.net/api?retryWrites=true&w=majority`  
+            }),
+            inject: [ConfigService]
+        }),
     ],
     exports: [MongooseModule]
 })
